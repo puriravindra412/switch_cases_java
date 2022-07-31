82f397865249e8064a87546924ed499296120595
puriravindra412 <70501907+puriravindra412@users.noreply.github.com>
2022-08-01 00:39:21 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
+/*
+switch case: Use the switch statement to select one of many code blocks to be executed
+
+switch(expression) {
+  case x:
+    // code block
+    break;
+  case y:
+    // code block
+    break;
+  default:
+    // code block
+}
+*/
+
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+             //input 
             Scanner sc= new Scanner(System.in);
             String fruits = sc.next();
+            //start switch
             switch (fruits){
                 case "mango"->System.out.println("king of fruits");
                 case "Apple"->System.out.println("Sweet red fruit");
@@ -11,6 +28,7 @@ public class Main {
 
 
             }
+           //end switch
 
     }
 }
